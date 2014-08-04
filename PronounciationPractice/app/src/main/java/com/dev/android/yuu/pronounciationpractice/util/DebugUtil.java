5f3cfdc99536d1dbe3469a03c11ff0ed81e4c959
YuuuuuYuuuuu <yuu.android.dev@gmail.com>
2014-08-04 22:33:14 +0900

    Add debug log function

--- PronounciationPractice/app/src/main/java/com/dev/android/yuu/pronounciationpractice/util/DebugUtil.java
+++ PronounciationPractice/app/src/main/java/com/dev/android/yuu/pronounciationpractice/util/DebugUtil.java
@@ -1,7 +1,22 @@
 package com.dev.android.yuu.pronounciationpractice.util;
 
+import android.util.Log;
+
 /**
  * Created by Chieko on 8/3/14.
  */
 public class DebugUtil {
+
+    static void DebugLog(String... args)
+    {
+        String msg = "";
+
+        for(String s : args)
+        {
+            msg += s + "-";
+        }
+
+        Log.d("DebugLog", msg);
+    }
+    
 }
